da645818ff2b3c602e3474be74be3999417f5790
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-02-26 20:17:20 +0200

    second commit

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blueBright('blue word'));
+console.log(chalk.green('green word'));
+console.log(chalk.magenta('magenta word'))
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,7 @@
+const name = "Galya";
+let age = 28;
+let isSunny = true;
+
+console.log("Homework from student "+name)
+console.log(age)
+console.log(isSunny)
