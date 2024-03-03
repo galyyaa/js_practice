bf968ce6f774058620a25f949fdbe9e711e404c8
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-03-03 18:00:05 +0200

    fix_third commit

--- js_basic_task2/task2.js
+++ js_basic_task2/task2.js
@@ -1,29 +1,35 @@
 let car_cost = 10000
 let car_year = 2020
-let final_cost
-let tax = 1
-let mileage = 65000
-let climate_control = true
-let security_package = true
+let tax = 0
+let mileage = 70000
+let climate_control = false
+let security_package = false
 let car_electric = true
 let message
+let final_cost = car_cost
 
-if (car_year>2019 && car_year<=2022){
-    final_cost = car_cost*(tax+0.1)
+if (car_year > 2019 && car_year <= 2022){
+    tax += 0.1
+    final_cost += car_cost * 0.1
     message = 'final cost increases by 10%, cause it`s a new car'
-} if (mileage>50000 || mileage<=100000){
-    final_cost = car_cost*(tax-0.1)
+} if (mileage >50000 && mileage <= 100000){
+    tax -= 0.05
+    final_cost += car_cost * 0.05
     message = 'final cost decreases by 5%, cause high mileage in the car'
-} if (climate_control=true){
-    final_cost = car_cost*(tax+0.07)
+} if (climate_control == true){
+    tax += 0.07
+    final_cost += car_cost * 0.07
     message = 'final cost increases by 7%, cause the car has climate control'
-} if (security_package=true){
-    final_cost = car_cost*(tax+0.08)
+} if (security_package == true){
+    tax += 0.08
+    final_cost += car_cost * 0.08
     message = 'final cost increases by 8%, cause car has security package'
-} if (car_electric=true){
-    final_cost = car_cost*(tax+0.15)
+} if (car_electric == true){
+    tax += 0.15
+    final_cost += car_cost * 0.15
     message = 'final cost increases by 15%, cause it`s electric car and less damage to our ecology'
 }
+final_cost = car_cost * (tax + 1)
 console.log(car_cost);
 console.log(final_cost);
 console.log(message)
