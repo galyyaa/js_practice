52154b67e5ff22ac06b17c57818bcab134f6ab95
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-03-13 20:23:26 +0200

    functions1 commit

---
+++ js_basic_task5/task1.js
@@ -0,0 +1,13 @@
+// 1. Напишіть функцію, яка приймає рядок в якості аргументу і повертає довжину цього рядка.
+
+function string() {
+    console.log("Homework".length)
+}
+string()
+
+// -----------------------------------
+
+let string2 = function (){
+    console.log("Functions".length)
+}
+string2()
