ba09b243cd34bbcc537f9a31d1d7293a76b6f0df
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-03-30 10:55:35 +0200

    async_fix commit

--- js_basic_task9/task1.js
+++ js_basic_task9/task1.js
@@ -1,22 +1,9 @@
 // За допомогою setTimeout створіть функцію, яка приймає рядок, та кількість секунд.
 // Функція повинна виводити переданий текст через передану кількість секунд.
 
-console.log("Wake up")
-
-setTimeout(function () {
-    console.log("In the morning: breakfast")
-}, 1000)
-
-setTimeout(function () {
-    console.log("In the afternoon: lunch")
-}, 2000)
-
-setTimeout(function () {
-    console.log("In the evening: dinner")
-}, 3000)
-
-setTimeout(function () {
-    console.log("Go to sleep")
-}, 4000)
-
-
+function printTextWithDelay(string, seconds) {
+    setTimeout(function(){
+        console.log(string)
+    }, seconds * 1000)
+}
+printTextWithDelay("Text with delay", 2)
