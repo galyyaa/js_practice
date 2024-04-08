423a4c47c024ffb346a42406658e9a1d71c5d3d7
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-04-08 21:55:58 +0300

    hillel-auto-tests commit

---
+++ mocha/test.js
@@ -0,0 +1,13 @@
+describe("Test describe", () => {
+
+    it ("Test1", () => {
+        console.log("test string 1");
+    });
+    it ("Test2", () => {
+        console.log("test string 2");
+    });
+    it ("Test3", () => {
+        console.log("test string 3");
+    });
+    
+})
